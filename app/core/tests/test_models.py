b50016cb9cebabf40a4bf0663057ebd3f50bb03b
kaywanbalbas <kaywanbalbas@gmail.com>
2020-09-22 16:55:47 -0700

    Raise ValueError if new user doesn't enter an email address

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,7 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = 'test@mail.com'
-        password = 'test'
+        password = 'test123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
@@ -19,7 +19,11 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'test@MAIL.COM'
-        user = get_user_model().objects.create_user(email, 'test')
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
